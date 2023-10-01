48b4456ab81b33cd3e7376d35bd1f99b50f2c523
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2023-10-01 15:02:50 +0200

    remove old

--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: import.meta.env.NG_APP_ENV,
-    auth0domain: import.meta.env.NG_APP_AUTH0_DOMAIN,
-    auth0client: import.meta.env.NG_APP_AUTH0_CLIENT_ID,
-    apiUrl: import.meta.env.NG_APP_API_BASE_URL,
-  };

be74c1f2f56b966f3cbf75bf1ebae5a50982d843
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2023-10-02 08:14:15 +0200

    feat: add http2 support

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,29 +2,52 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+import 'tslib';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import express from 'express';
+import { ServerOptions, createServer } from 'spdy';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  app.enableCors();
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+    const certs = join(__dirname, './assets/certs');
+    // const httpsOptions = {
+    //     key:  readFileSync(`${certs}/key.pem`),
+    //     cert: readFileSync(`${certs}/cert.pem`),
+    // }
+    const ops: ServerOptions = {
+        key:  readFileSync(`${certs}/key.pem`),
+        cert: readFileSync(`${certs}/cert.pem`),
+    }
+    // const app = await NestFactory.create(AppModule, { httpsOptions });
+    const expressApp = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    const server = createServer(ops, expressApp)
 
-// const server = createServer({
-//     key:  readFileSync(`${CERTIFICATES}/key.pem`),
-//     cert: readFileSync(`${CERTIFICATES}/cert.pem`),
-// }, app.callback())
-// .listen(443);
+    // Used for giving a global namespace: http:localhost/api
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
 
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+    app.enableCors();
+    console.log(process.env.PORT);
+    const port = process.env.PORT || 443;
+    // await app.listen(port);
+
+    await app.init();
+    await server.listen(port);
+
+    // const server = createServer({
+
+    // }, app.callback())
+    // .listen(443);
+
+    Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    );
 }
 
 bootstrap();

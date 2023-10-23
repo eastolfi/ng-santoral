dfae36b6562467df29fdf68a817de536398d98d1
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2023-10-23 22:02:45 +0200

    feat: add pwa support

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthModule as Auth0Module } from '@auth0/auth0-angular';
@@ -10,6 +10,7 @@ import { environment } from '@frontend/envs/environment';
 import { FeaturesModule } from './features/features.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
     declarations: [AppComponent],
@@ -26,6 +27,12 @@ import { AppComponent } from './app.component';
         SharedModule,
         FeaturesModule,
         AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
     ],
     providers: [AuthService],
     bootstrap: [AppComponent],

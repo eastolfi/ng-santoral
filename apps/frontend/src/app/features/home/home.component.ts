8133e643d489a1dd9a8e064707deba058c0c8943
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2024-02-18 09:54:56 +0100

    chore: tmp

--- apps/frontend/src/app/features/home/home.component.ts
+++ apps/frontend/src/app/features/home/home.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { environment } from '@frontend/envs/environment';
 
+// https://www.reddit.com/r/docker/comments/yk0x2l/cors_error_after_dockerizing_how_to_fix/?rdt=47139
 @Component({
     selector: 'snt-home',
     templateUrl: './home.component.html',
@@ -11,10 +12,16 @@ export class HomeComponent {
     public options = [
         // environment.apiUrl,
         '/api',
+        '/rest',
+        '/resta',
+        '/restb',
         // 'https://192.168.1.119:2443/api',
         'https://api/api',
         'https://api:2443/api',
-        'api:2443/api',
+        'https://rest',
+        'https://rest/api',
+        'https://rest:2443',
+        'https://rest:2443/api',
         // '/api/v1',
         // '/api/v2',
         // '/api',
